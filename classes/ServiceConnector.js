a6efa18186da99eb37422d875e8128fc26b4188f
jinu-at-659187677743 <jinujosephdaniel@gmail.com>
2023-05-11 01:50:23 +0530

    service connector class changed

--- classes/ServiceConnector.js
+++ classes/ServiceConnector.js
@@ -2,13 +2,15 @@ import { ServiceConnectAdapter } from "./ServiceConnectAdapter.js";
 import { Service } from "./Service.js";
 export class ServiceConnector {
     adapter = new ServiceConnectAdapter()
-    constructor(adapter = new ServiceConnectAdapter()) {
+    serviceInfo = new Object()
+    constructor(adapter = new ServiceConnectAdapter(), serviceInfo = new Object()) {
         this.adapter = adapter
+        this.serviceInfo = serviceInfo
     }
-    connect(service = new Service()) {
-        return this.adapter.connect(service)
+    call(fnName = "", parameters = []) {
+        return this.adapter.call(fnName, parameters, this.serviceInfo)
     }
     publish(service = new Service()) {
-        return this.adapter.publish(service)
+        return this.adapter.publish(service, this.serviceInfo)
     }
 }
